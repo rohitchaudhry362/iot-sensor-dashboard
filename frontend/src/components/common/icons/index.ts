4d075a8cfe6e8811c079a5c038cc2fe1f4d7eb0c
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2026-09-19 16:32:21 -0400

    add icons

---
+++ frontend/src/components/common/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CheckCircleRounded } from '@mui/icons-material/CheckCircleRounded';
+export { default as LogoutRounded } from '@mui/icons-material/LogoutRounded';
+export { default as Visibility } from '@mui/icons-material/Visibility';
+export { default as VisibilityOff } from '@mui/icons-material/VisibilityOff';
