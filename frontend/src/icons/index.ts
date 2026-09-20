56cc6158d47295da819ff891a98db46426c01186
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2026-09-20 18:12:37 -0400

    add icons

--- frontend/src/icons/index.ts
+++ frontend/src/icons/index.ts
@@ -2,6 +2,7 @@ export { default as CheckCircleRounded } from '@mui/icons-material/CheckCircleRo
 export { default as DirectionsRunRounded } from '@mui/icons-material/DirectionsRunRounded';
 export { default as DoorFrontRounded } from '@mui/icons-material/DoorFrontRounded';
 export { default as LogoutRounded } from '@mui/icons-material/LogoutRounded';
+export { default as SensorsRounded } from '@mui/icons-material/SensorsRounded';
 export { default as ThermostatRounded } from '@mui/icons-material/ThermostatRounded';
 export { default as Visibility } from '@mui/icons-material/Visibility';
 export { default as VisibilityOff } from '@mui/icons-material/VisibilityOff';
